253d6d909c6a22a3ddac3460241f479f89c3658d
Jim <jjfoutlooke@gmail.com>
2020-05-21 14:12:02 -0400

    New change to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi -again! 
+          Hi -again - Feature branch! 
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
